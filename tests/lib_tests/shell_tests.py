8c8d2db4c965967bc5b5138467b8467020440a65
serge <serge-olivier.paquette@delvelabs.ca>
2017-11-01 10:57:08 -0400

    Good test case for timeout with errors.

--- tests/lib_tests/shell_tests.py
+++ tests/lib_tests/shell_tests.py
@@ -1,5 +1,7 @@
 import unittest
 import time
+import os
+import subprocess
 from mock import patch
 from core.lib.shell import CommandExecutor
 
@@ -18,15 +20,17 @@ class ExecutorTest(unittest.TestCase):
 
 
     def test_command_timeout_with_results(self):
-        cmd = ['sleep', '10']
-        executor = CommandExecutor(cmd)
+        cmd = ['tail', '-f', os.path.realpath(__file__)]
+        executor = CommandExecutor(cmd, stderr=True)
         result = executor.execute(1)
 
-        self.assertEqual(result, '')
+        self.assertIn("result", result[0])
 
-    # def test_command_timeout_without_results(self):
-    #     cmd = ['sleep', '10']
-    #     executor = CommandExecutor(cmd)
-    #     result = executor.execute(1)
-    #
-    #     self.assertEquals(result, '')
+    @patch.object(subprocess.Popen, 'communicate')
+    def test_command_timeout_with_errors(self, mock_comm):
+        mock_comm.return_value= (None, "error")
+        cmd = ['sleep', '10']
+        executor = CommandExecutor(cmd, stderr=True)
+        result = executor.execute(1)
+
+        self.assertEquals("error", result[1])
