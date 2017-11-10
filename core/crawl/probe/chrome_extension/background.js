1c2e16982df8b1492f0024c7e7e591ebffea72df
Guillaume Grasset <guillaume@delvelabs.ca>
2017-11-10 10:39:38 -0500

    make possible to retrieve the blocked url from the extension

--- core/crawl/probe/chrome_extension/background.js
+++ core/crawl/probe/chrome_extension/background.js
@@ -1,23 +1,68 @@
 (function() {
     'use strict';
 
-    let isStartingUrlNavigated = false,
-        startingUrl;
+    // keep track of all the opened tab
+    let tabs = {};
 
-    chrome.webRequest.onBeforeRequest.addListener((details) => {
+    // Get all existing tabs
+    chrome.tabs.query({}, function(results) {
+        results.forEach(function(tab) {
+            tabs[tab.id] = tab;
+        });
+    });
 
-        if (!isStartingUrlNavigated && details.type === 'main_frame') {
-            // DEBUG:
-            console.warn(`First navigation to ${details.url} done.`);
-            isStartingUrlNavigated = true;
-            startingUrl = details.url;
-        } else if (['main_frame', 'sub_frame'].includes(details.type) && details.url !== startingUrl) {
+
+    // Create tab event listeners
+    function onUpdatedListener(tabId, changeInfo, tab) {
+        tabs[tab.id] = tab;
+    }
+
+    function onRemovedListener(tabId) {
+        delete tabs[tabId];
+    }
+
+    /**
+     * if the request url differ from the current tab url block it
+     * @param details
+     * @return {{redirectUrl: string}}
+     */
+    function onBeforeRequestListener(details) {
+        let currentTab = tabs[details.tabId];
+
+        if (currentTab.url.startsWith('http') && _compareUrls(details.url, currentTab.url)) {
             // DEBUG:
             console.warn(`Navigation to ${details.url} blocked.`);
+            chrome.tabs.executeScript(details.tabId, {file: 'content.js'}, function() {
+                chrome.tabs.sendMessage(details.tabId, {url: details.url});
+            });
             return {redirectUrl: 'javascript:void(0)'};
         }
-    },
-    {urls: ['<all_urls>']},
-    ['blocking']);
+    }
+
+    // Subscribe to tab events to track opened tabs
+    chrome.tabs.onUpdated.addListener(onUpdatedListener);
+    chrome.tabs.onRemoved.addListener(onRemovedListener);
+
+    chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestListener, {
+        urls: ['<all_urls>'],
+        types: ['main_frame', 'sub_frame'], // only watching for frame type request
+    }, ['blocking']);
+
+    /**
+     * compare 2 urls based on there href form WITHOUT the hash part
+     * @param {string} url1
+     * @param {string} url2
+     * @return {boolean}
+     * @private
+     */
+    function _compareUrls(url1, url2) {
+        let cleanedUrl1 = new URL(url1),
+            cleanedUrl2 = new URL(url2);
+
+        cleanedUrl1.hash = '';
+        cleanedUrl2.hash = '';
+
+        return cleanedUrl1.href !== cleanedUrl2.href;
+    }
 
 })();
